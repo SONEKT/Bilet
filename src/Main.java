6837e3731723800e567755d430d8090a6a1534a4
SONEKT <shulmanlevgen@gmail.com>
2023-06-23 15:01:01 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] numbers = {2, 3, 31, 5, 15, 61, 1, 25};
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 != 0) {
+                numbers[i] ++;
+            }
+        }
+        for (int i = 2; i < 7; i++) {
+            System.out.println(numbers[i]);
+        }
     }
 }
