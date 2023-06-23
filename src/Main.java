39e510088ee66816ff795969f12a6255e02b0c1e
SONEKT <shulmanlevgen@gmail.com>
2023-06-23 15:23:36 +0500

    1

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = {2, 3, 31, 5, 15, 61, 1, 25};
+        int[] numbers = {1, 2, 3, 4, 12, 45, 567, 12};
         for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 != 0) {
+            if (numbers[i] % 2 == 0) {
                 numbers[i] ++;
             }
         }
-        for (int i = 2; i < 7; i++) {
+        for (int i = 2; i <7; i++){
             System.out.println(numbers[i]);
         }
     }
